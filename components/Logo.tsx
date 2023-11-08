2318fa6b32858956f04ac292e4be9018ff090976
iamrajsingh <divyraj65407@gmail.com>
2023-11-09 00:35:07 +0530

    prisma schema done

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,8 +1,9 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Logo() {
   return (
-    <div>Logo</div>
+    <Link href={"/"} className='font-bold text-2xl bg-gradient-to-t from-indigo-400 to-cyan-400 text-transparent bg-clip-text font-sans hover:cursor-pointer'>FormShare<span>360</span></Link>
   )
 }
 
