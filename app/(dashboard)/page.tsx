5e0a37b0fd452dfad7e2bfc9e7888c730884a5c6
iamrajsingh <divyraj65407@gmail.com>
2023-11-23 01:03:06 +0530

    create form done

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -7,6 +7,7 @@ import { FaWpforms } from "react-icons/fa";
 import { HiCursorClick } from "react-icons/hi";
 import { TbArrowBounce } from "react-icons/tb";
 import { Separator } from "@/components/ui/separator";
+import CreateFormBtn from "@/components/CreateFormBtn";
 
 export default function Home() {
   return (
@@ -17,6 +18,9 @@ export default function Home() {
       <Separator className="my-6"/>
       <h2 className="text-4xl font-bold col-span-2">Your Forms</h2>
       <Separator className="my-6"/>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <CreateFormBtn/>
+      </div>
     </div>
   );
 }
@@ -112,3 +116,4 @@ function StatsCard({
     </Card>
   );
 }
+
